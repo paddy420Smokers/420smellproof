dd64fbf87ee60c2e37b3447160025b53195d969c
Patrick <p.meier2016@outlook.com>
2020-04-05 15:26:45 +0300

    Navbar added as well as basic css

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,8 +10,9 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import "./bootstrap.min.css";
 import "./layout.css"
+import Navbar from "./Globals/Navbar";
 
-const Layout = ({ children }) => <>{children}</>;
+const Layout = ({ children }) => <> <Navbar/> {children}</>;
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
