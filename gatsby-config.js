3d5ff05555a2586562aa6c5e133e30e093ec3688
Patrick <p.meier2016@outlook.com>
2020-04-05 14:15:31 +0300

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `420SmellProof`,
+    description: `We review the best smell proof bags on the market for 2020`,
+    author: `@patrick`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
